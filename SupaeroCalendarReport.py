5076a0d3b594e247f8cadae6642ddf1efecbbd44
Julien P <julienprissi@protonmail.com>
2019-10-20 12:48:26 +0200

    Small fixes : Added a line for unicode support in GNU/Linux Fixed the README.md as the code only works for Python>=3.2 because of the module 'timezone'

--- SupaeroCalendarReport.py
+++ SupaeroCalendarReport.py
@@ -1,3 +1,4 @@
+# coding: utf8
 """
 "THE BEER-WARE LICENSE" (Revision 42):
 Remy CHATEL <remychatel@fastmail.com> wrote this file.  As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
